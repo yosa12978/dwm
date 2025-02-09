46592dd88c3552d45965d4a11eaaf5d195255fbc
yosa12978 <yosa12978@gmail.com>
2025-02-09 11:49:47 +0300

    changed colorscheme

--- config.h
+++ config.h
@@ -10,10 +10,10 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
+static const char col_gray1[]       = "#101421";
+static const char col_gray2[]       = "#323643";
+static const char col_gray3[]       = "#a9aeba";
+static const char col_gray4[]       = "#dce1ed";
 static const char col_primary[]     = "#a47de9";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
