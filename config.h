8cebd6a7da025f86eb0871baceda8d5fa97e221e
yosa12978 <yosa12978@gmail.com>
2025-02-15 19:09:30 +0300

    fixed dwm swallow issue

--- config.h
+++ config.h
@@ -36,8 +36,9 @@ static const Rule rules[] = {
 	/* class       instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
 	{ "Gimp",      NULL,     NULL,           0,         1,          0,           0,        -1 },
 	{ "Firefox",   NULL,     NULL,           0,         0,          0,          -1,        -1 },
-	{ "Chromium",  NULL,     NULL,           0,         0,          0,          -1,        -1 },
-    { "St",        NULL,     NULL,           0,         0,          1,           0,        -1 },
+	{ "Chromium",  NULL,     NULL,           0,         0,          0,          -1,        -1 }, 
+	{ "Brave",     NULL,     NULL,           0,         0,          0,          -1,        -1 },
+    { "xterm-256color", NULL,NULL,           0,         0,          1,           0,        -1 },
     { "Alacritty", NULL,     NULL,           0,         0,          1,           0,        -1 },
 	{ NULL,        NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 };
